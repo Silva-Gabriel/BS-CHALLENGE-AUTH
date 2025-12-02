87b65ac5d1089bb7b514d24a57716f737cfa90f2
Gabriel S S <pottergryff8@gmail.com>
2025-12-01 22:18:56 -0300

    fix: adjustment for build

--- api/src/AuthController.cs
+++ api/src/AuthController.cs
@@ -1,7 +1,9 @@
+using System.Threading.Tasks;
 using application.auth;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace api.src.controllers
 {
